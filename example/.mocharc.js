c34a329eefea4a462bc43d41d2a1a5bf13413b0c
Josh White <josh.white@talkthinkdo.com>
2022-10-24 17:12:53 +0100

    chore: further package.json prep

--- example/.mocharc.js
+++ example/.mocharc.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   reporter: "azure-devops-mocha-reporter",
-  // "reporter-option": [
-  //   `pat=${process.env.PAT}`,
-  //   `organisation=${process.env.ORGANISATION}`,
-  //   `project=${process.env.PROJECT}`,
-  //   `planId=${process.env.PLAN_ID}`,
-  //   `runName=${process.env.RUN_NAME}`,
-  // ],
+  "reporter-option": [
+    `pat=${process.env.PAT}`,
+    `organisation=${process.env.ORGANISATION}`,
+    `project=${process.env.PROJECT}`,
+    `planId=${process.env.PLAN_ID}`,
+    `runName=${process.env.RUN_NAME}`,
+  ],
 };
